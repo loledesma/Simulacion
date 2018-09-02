34a4ad3afa31f4fc86eb345dee2e3d177d790e11
FacundoForcada <facundoforcada.ff@gmail.com>
2018-09-01 21:27:05 -0300

    clases e interfaces agregadas.

---
+++ TrabajoPractico3/TrabajoPractico3/Generadores/AleatorioSistema.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPractico3.Generadores
+{
+    public class AleatorioSistema : IGeneradores
+    {
+        public double Generar()
+        {
+            var _random = new Random().NextDouble();
+            return _random;
+        }
+
+        public int Generar(int cifras)
+        {
+            var _random = Generar();
+            return (int)(_random * Math.Pow(10, cifras));
+        }
+    }
+}
---
+++ TrabajoPractico3/TrabajoPractico3/Generadores/CongruencialMultiplicativo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPractico3.Generadores
+{
+    public class CongruencialMultiplicativo : IGeneradores
+    {
+        public double _seed { get; protected set; }
+        public double _a { get; protected set; }
+        public double _m { get; protected set; }
+
+        public CongruencialMultiplicativo(double seed, double a, double m)
+        {
+            _seed = seed;
+            _a = a;
+            _m = m;
+        }
+        public double Generar()
+        {
+            var _random = (_a * _seed) % _m;
+            _seed = _random;
+            return _random / _m;
+        }
+        public int Generar(int cifras)
+        {
+            var _random = Generar();
+            return (int)(_random * Math.Pow(10, cifras));
+        }
+    }
+}
