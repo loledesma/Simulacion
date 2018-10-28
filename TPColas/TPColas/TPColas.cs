33ce18ea5e8589bcdeb3802286105fe85b440ba8
loledesma <lorenasoledadledesma@gmail.com>
2018-10-28 12:05:32 -0300

    Confirmar

--- TPColas/TPColas/TPColas.cs
+++ TPColas/TPColas/TPColas.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿
+using NumerosAleatorios;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+
+
 
 namespace TPColas
 {
