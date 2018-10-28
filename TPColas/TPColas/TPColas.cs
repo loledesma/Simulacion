3ffa43d4c9a965d3efc5c8d3cfc54fd192296b45
FacundoForcada <facundoforcada.ff@gmail.com>
2018-10-28 11:38:10 -0300

    Cambios en Estructura

--- TPColas/TPColas/TPColas.cs
+++ TPColas/TPColas/TPColas.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TPColas
 {
-    public partial class Form1 : Form
+    public partial class TPColas : Form
     {
-        public Form1()
+        public TPColas()
         {
             InitializeComponent();
         }
